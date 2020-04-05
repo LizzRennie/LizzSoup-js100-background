0f6904e5a91774eab1c539329d5d9cda16219ec1
lizz <elizabeth.ann.soup@gmail.com>
2020-04-05 01:36:21 -0400

    Changed copy

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const h1 = document.querySelector('h1');
 const balloon = document.querySelectorAll('.balloon');
+const balloonHbd = document.querySelectorAll('.hbd');
 const message = document.getElementById('message');
 
 let counter = 0;
@@ -8,31 +9,37 @@ let counter = 0;
 balloon.forEach((balloon) => {
   balloon.addEventListener('click', () => {
     counter++;
-    bdayMessage(); 
     balloon.style.background = '#111';
     balloon.innerHTML = 'POP!';
-    console.log('counter'); 
+    bdayMessage(); 
   });
 });
 
 function bdayMessage() {
   if (counter === 10) {
+    message.style.color = 'palegoldenrod';
     balloon.forEach((balloon) => {
       balloon.innerHTML = 'HAPPY!';
     })
   } else if (counter === 20) {
+    message.style.color = 'lightblue';
     balloon.forEach((balloon) => {
-      balloon.innerHTML = '50th';
+      balloon.innerHTML = 'Fucking';
     });
   } else if (counter === 30) {
+    message.style.color = 'lavender';
     message.innerHTML = 'Happy Birthday!';
     message.style.color = 'turquoise';
     balloon.forEach((balloon) => {
       balloon.innerHTML = 'BIRTHDAY!';
     });
+  } else if (counter === 31) {
+    message.style.color = 'pink';
+    balloonHbd.forEach((balloonHbd) => {
+      balloonHbd.innerHTML = '🎉';
+    });
   } else if (counter > 31) {
-    message.style.color = 'hotpink';
-    message.innerHTML = 'Birthday 50th Birthday!';
+    message.style.color = 'turquoise';
     balloon.forEach((balloon) => {
       balloon.style.display = 'none';
     });
