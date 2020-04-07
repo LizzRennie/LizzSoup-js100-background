511a3d0fa5973134dfa7ee2c9dee9e484f8838e5
lizz <elizabeth.ann.soup@gmail.com>
2020-04-06 22:06:49 -0400

    Added input feature

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const body = document.querySelector('body');
-const h1 = document.querySelector('h1');
 const balloon = document.querySelectorAll('.balloon');
 const message = document.getElementById('message');
+const name = document.getElementById('name');
+const subBtn = document.getElementById('subBtn');
 
 let counter = 0;
 
+subBtn.addEventListener('click', submit);
+
+function submit() {
+  const container = document.querySelector('.container');
+  const form = document.getElementById('form');
+  container.classList.remove('hidden');
+  form.classList.add('hidden');
+  console.log('it worked' + form + container);
+}
+
 balloon.forEach((balloon) => {
   balloon.addEventListener('click', () => {
-    counter++;
     bdayMessage(); 
+    counter++;
     balloon.style.background = '#111';
     balloon.innerHTML = 'POP!';
     console.log('counter'); 
@@ -16,23 +26,28 @@ balloon.forEach((balloon) => {
 });
 
 function bdayMessage() {
+  const hbd = document.querySelectorAll('.hbd');
+  let bdayName = name.value;
   if (counter === 10) {
     balloon.forEach((balloon) => {
       balloon.innerHTML = 'HAPPY!';
     })
   } else if (counter === 20) {
     balloon.forEach((balloon) => {
-      balloon.innerHTML = '50th';
+      balloon.innerHTML = 'Birthday';
     });
-  } else if (counter === 30) {
-    message.innerHTML = 'Happy Birthday!';
+  } else if (counter === 29) {
+    message.innerHTML = `Happy Birthday ${bdayName}!`;
     message.style.color = 'turquoise';
     balloon.forEach((balloon) => {
-      balloon.innerHTML = 'BIRTHDAY!';
+      balloon.innerHTML = `🥳`;
     });
-  } else if (counter > 31) {
+    hbd.forEach((hbd) => {
+      hbd.innerHTML = 'HBD! 🎉';
+    });
+  } else if (counter === 30) {
     message.style.color = 'hotpink';
-    message.innerHTML = 'Birthday 50th Birthday!';
+    message.innerHTML = `Time to celebrate! 🎉`;
     balloon.forEach((balloon) => {
       balloon.style.display = 'none';
     });
